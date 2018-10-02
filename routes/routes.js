0eb0b336df132118eeb1c4666db029b0b7cb34fb
Bao Pham <bdpham00@gmail.com>
2018-10-01 23:07:50 -0700

    Added CORS for localhost:4200

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,12 @@
 var service = require('../service/service');
 
 var appRouter = function (app) {
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+
     app.get("/", function(req, res) {
       res.status(200).send("Welcome to our restful API");
     });
