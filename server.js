06b9443a8f36afc06dbad22ef6412155daa6083f
bdpham <bdpha@DESKTOP-M4UUU5N>
2018-10-01 18:41:54 -0700

    Working GET request

--- server.js
+++ server.js
@@ -1,26 +1,9 @@
 var http = require('http');
-
-var options = {
-    host: 'api.fortnitetracker.com', 
-    port: 80,
-    path: 'https://jsonplaceholder.typicode.com/posts',
-    method: 'GET'
-}
-
-http.request(options, function(res){
-    var body = ''; 
-
-    res.on('data', function(chunk){
-        body += chunk;
-    });
-
-    res.on('end', function() {
-        var json = JSON.parse(body);
-        console.log(json);
-    })
-});
+var service = require('./service/service');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
 }).listen(8080);
+
+service.getFortniteData(); 
---
+++ service/service.js
@@ -0,0 +1,15 @@
+var Client = require('node-rest-client').Client;
+
+var client = new Client();
+
+exports.getFortniteData = function() {
+    //Set Fortnite Header
+    var args = {
+        headers: { "TRN-Api-Key":  "f3ac61b5-aa77-44be-877f-f21b1c2914fc" }
+    }
+    // direct way
+    client.get("https://api.fortnitetracker.com/v1/profile/pc/imstillyoung", args, function (data, response) {
+        // parsed response body as js object
+        console.log(data);
+    });
+}
